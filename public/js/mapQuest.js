f24f87dcb338f80955f5badf0f231e472290bdfc
Lannie Tran <lht009@ucsd.edu>
2021-02-25 21:44:28 -0800

    lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+		L.mapquest.key = 'KgGmHvxoAZcp2HyV9goq7nUbaDNcbIyG';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.87864529499566, -117.23589424154437],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// Add a market to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
